855cd982d7e85fd83da2b6a26dfe19db97dffd42
YoussufSalah <youssufsalahsaber@gmail.com>
2025-08-21 12:59:31 +0300

    Added simple error handler middleware

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ const app = express();
 import indexRoutes from "./routes/index.routes.js";
 app.use(indexRoutes);
 
+app.use((err, req, res, next) => {
+    res.status(500).json({ status: "error", error: err });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server is listening on port", PORT));
